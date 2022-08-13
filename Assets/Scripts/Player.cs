fbe102746ae68d8a0269934815ef2d2731335fcd
Cainiam <boisdenghien@gmail.com>
2022-08-13 22:01:09 +0200

    114. Boundaries
    - Adding boundaries on the screen using ViewportToWorldPoint on the main camera
    - Adding paddings to stop player to be sliced and to have top and bottom space for UI and some choose of game design (for top spacing especially)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,18 +5,44 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [Header("Player Movement")]
     [SerializeField] float moveSpeed = 5f;
     Vector2 rawInput;
+
+    //We use some paddings because the track of the vessel is on his pivot (on the middle) and we want it to not be sliced in 2 on the border of the screen,
+    //We also want to add padding to add UI information (on the bottom of the screen) and limit where the player can go to the top of the screen (as a game design choose)
+    [Header("Screen Management")]
+    [SerializeField] float paddingLeft;
+    [SerializeField] float paddingRight;
+    [SerializeField] float paddingTop;
+    [SerializeField] float paddingBottom;
+    Vector2 minBounds;
+    Vector2 maxBounds;
+
+    void Start() 
+    {
+        InitBounds();    
+    }
 
     void Update()
     {
         Move();
     }
 
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0,0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1,1));
+    }
+
     void Move()
     {
-        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
-        transform.position += delta;
+        Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop);
+        transform.position = newPos;
     }
 
     void OnMove(InputValue value)
