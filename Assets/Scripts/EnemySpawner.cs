faea536d03b53408f0440f627a52c5c7727c862f
Cainiam <boisdenghien@gmail.com>
2022-08-14 21:04:41 +0200

    119. While Loops
    Adding a do-while loop with a boolean condition (in our Spawner Script) for ennemy wave to spawn  indefinitely for now

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -8,7 +8,9 @@ public class EnemySpawner : MonoBehaviour
     [Header("Waves Configuration for Enemies Spawn")]
     [SerializeField] List<WaveConfigSO> waveConfigs;
     [SerializeField] float timeBetweenWaves = 0f;
+    [SerializeField] bool isLooping;
     WaveConfigSO currentWave;
+    
 
     void Start()
     {
@@ -22,19 +24,23 @@ public class EnemySpawner : MonoBehaviour
 
     IEnumerator SpawnEnemyWaves()
     {
-        foreach(WaveConfigSO wave in waveConfigs)
+        do 
         {
+            foreach(WaveConfigSO wave in waveConfigs)
+            {
             currentWave = wave;
             for(int i = 0; i < currentWave.GetEnemyCount(); i++)
-            {
-            Instantiate(currentWave.GetEnemyPrefab(i), 
+                {
+                Instantiate(currentWave.GetEnemyPrefab(i), 
                     currentWave.GetStartingWaypoint().position,
                     Quaternion.identity, //Quaternion.identity cause no rotation to our object
                     transform); // to spawn enemies under "EnemySpawner" GameObject
-            yield return new WaitForSeconds(currentWave.GetRandomSpawnTime()); //coroutine to have time between Enemies spawn
+                yield return new WaitForSeconds(currentWave.GetRandomSpawnTime()); //coroutine to have time between Enemies spawn
+                }
+                yield return new WaitForSeconds(timeBetweenWaves); //coroutine to have time between Wave spawn
             }
-            yield return new WaitForSeconds(timeBetweenWaves); //coroutine to have time between Wave spawn
-        }
+        } while(isLooping);
+        
         
     }
 }
